ff79c6a354559562d21224667ba3e917646c0aae
Muhammad Hashim <muhammadhashim10037@gmail.com>
2024-09-19 22:59:33 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+alert("Welcome To Our Website.");
+alert("Error!Please enter a valid password.");
+alert("Welcome to JS Land...\nHappy Coding!");
+alert("Welcome to JS Land...");
+alert("Prevent this page from creating additional dialogs.");
+alert("Hello...I can run JS through my web browser's console");
